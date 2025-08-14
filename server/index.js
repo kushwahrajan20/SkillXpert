5f9adc8fd2f513bd7b49109042555ff22617fd3d
krajan2006 <kushawahaharikishan1@gmail.com>
2025-08-14 18:07:54 +0530

    change

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://skill-xpert.vercel.app/"], // Update with your client URL
+  origin: ["http://localhost:3000", "https://skill-xpert.vercel.app"], // Update with your client URL
   credentials: true,
 }));
 
